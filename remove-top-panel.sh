477876ce476776b9dd452ab391471c192158ab4c
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2016-12-08 16:05:34 +0100

    ajout d'une condition pour resize le panel droit s'il est actif pendant qu'on ferme les panneaux supperieurs

--- remove-top-panel.sh
+++ remove-top-panel.sh
@@ -6,3 +6,11 @@ herbstclient remove_monitor onTopVirtualRight
 
 # Remise du padding a zero pour le panneau principal
 herbstclient pad develop 16
+
+# Si le virtual screen de droite est actif et qu'on supprime les virtuals screens du haut alors,
+# on resize le panning de droite pour ne pas avoir de vide
+if [ -z "$(herbstclient list_monitors | grep -i onrightvirtualbottom | cut -d ',' -f 2 | cut -d '"' -f 2)" ]; then
+	herbstclient	pad			onRightVirtualBottom	16		0		0		0
+fi
+
+exit 0
