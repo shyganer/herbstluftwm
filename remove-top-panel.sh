7afd5e4de803409b0ea2dc5b9ac1bbfa8f45f74c
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2016-12-08 21:37:55 +0100

    conversion du code source avec les nouvelles fonctions

--- remove-top-panel.sh
+++ remove-top-panel.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+source /home/shyganer/b2c/config/herbstluftwm/musthave_functions.sh
 
 # Suppression des deux panneaux au dessus du principal
-herbstclient remove_monitor onTopVirtualLeft
-herbstclient remove_monitor onTopVirtualRight
+remove_panel onTopVirtualLeft
+remove_panel onTopVirtualRight
 
 # Remise du padding a zero pour le panneau principal
-herbstclient pad develop 16
+padding_panel develop 16
 
-# Si le virtual screen de droite est actif et qu'on supprime les virtuals screens du haut alors,
-# on resize le panning de droite pour ne pas avoir de vide
-if [ -z "$(herbstclient list_monitors | grep -i onrightvirtualbottom | cut -d ',' -f 2 | cut -d '"' -f 2)" ]; then
-	herbstclient	pad			onRightVirtualBottom	16		0		0		0
+# Si le panel de droit est actif, faire un padding de celui ci
+# depuisl le up pour supprimer le chevauchement
+if "$(is_panel_active onRightVirtualBottom)" = true; then
+	padding_panel onRightVirtualBottom 16
 fi
 
 exit 0
