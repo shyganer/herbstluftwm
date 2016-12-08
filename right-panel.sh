9265c0096261d589e77482a1f5ec13ecc6001447
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2016-12-08 21:24:13 +0100

    correction de la fonction is_panel_active dans right-panel.sh

--- right-panel.sh
+++ right-panel.sh
@@ -16,9 +16,9 @@ padding_panel	develop 16 384
 # Si le virtual screen en haut a droite est actif alors on ajoute un padding de 200 pixels supplementaires,
 # Sinon on fait seulement un padding de 16 pixels pour dzen
 if $(is_panel_active onTopVirtualRight) = true ; then
-	padding_panel onRightVirtualBottom 16
-else
 	padding_panel onRightVirtualBottom 216 0 0 0
+else
+	padding_panel onRightVirtualBottom 16
 fi
 
 exit 0
