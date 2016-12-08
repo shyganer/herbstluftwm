0014c5eb9deb4b362fc1a004a698928fe952a9e4
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2016-12-08 21:32:07 +0100

    correction pour l'utilisation de la fonction is_panel_active'

--- manage-top-panel.sh
+++ manage-top-panel.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -z "$(herbstclient list_monitors | grep -i ontopvirtualleft | cut -d ',' -f 2 | cut -d '"' -f 2)" ]; then
-	/home/shyganer/b2c/config/herbstluftwm/top-panel.sh
-else
+source /home/shyganer/b2c/config/herbstluftwm/musthave_functions.sh
+
+if $(is_panel_active onTopVirtualLeft) = true; then
 	/home/shyganer/b2c/config/herbstluftwm/remove-top-panel.sh
+else
+	/home/shyganer/b2c/config/herbstluftwm/top-panel.sh
 fi
 
 exit 0
