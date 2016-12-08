dcd0c54e567bbde3b38ab096fc10247b906af23f
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2016-12-08 21:42:23 +0100

    ajout du cas panneaux superieurs et droit ouvert puis fermeture panneau droit

--- remove-right-panel.sh
+++ remove-right-panel.sh
@@ -5,7 +5,13 @@ source /home/shyganer/b2c/config/herbstluftwm/musthave_functions.sh
 # Suppression du panneau au droit du principal
 remove_panel onRightVirtualBottom
 
-# Remise du padding a zero pour le panneau principal
-padding_panel develop 16 0 0 0
+# Si le panneau de droite est present ainsi que les panneaux
+# supperieur, lors de la suppression de ce premier
+# ajouter les 200 pixels supplementaires.
+if $(is_panel_active onTopVirtualLeft) = true; then
+	padding_panel develop 216 0 0 0
+else
+	padding_panel develop 16 0 0 0
+fi
 
 exit 0
