ffabe33c9c4c71d793bac75c9bdb75af8a2bab10
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2016-12-08 14:45:22 +0100

    on ajout un padding de 200 pixels supplementaire si le virtual screen en haut a droit est actif

--- right-panel.sh
+++ right-panel.sh
@@ -8,7 +8,16 @@ herbstclient add_monitor 384x1080+1536+0 rbtm onRightVirtualBottom
 
 # On ajoute les differents padding pour que les virtuals screens ne se chevauchent pas
 # client		function	name					padding
-herbstclient	pad			develop					16 384
-herbstclient	pad			onRightVirtualBottom	16
+#													up		right	bottom	left
+# 384 + 16 = 400
+herbstclient	pad			develop					16		384
+
+# Si le virtual screen en haut a droite est actif alors on ajoute un padding de 200 pixels supplementaires,
+# Sinon on fait seulement un padding de 16 pixels pour dzen
+if [ -z "$(herbstclient list_monitors | grep -i ontopvirtualright | cut -d ',' -f 2 | cut -d '"' -f 2)" ]; then
+	herbstclient	pad			onRightVirtualBottom	16		0		0		0
+else
+	herbstclient	pad			onRightVirtualBottom	216		0		0		0
+fi
 
 exit 0
