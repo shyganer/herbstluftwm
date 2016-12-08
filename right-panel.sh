e42eedc15eb9b8c376034981c3a5f5abe36a1d73
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2016-12-08 16:27:02 +0100

    conversion du code source avec les fonctions crees

--- right-panel.sh
+++ right-panel.sh
@@ -80,32 +80,45 @@ resize_panel ()
 
 is_panel_active ()
 {
-	if [ -z "" ]; then
+	name=$1
+
+	if [ -z "$(herbstclient list_monitors | grep -i $name | cut -d ',' -f 2 | cut -d '"' -f 2)" ]; then
+		unset name
 		return 1
 	else
+		unset name
 		return 0
 	fi
 }
 
 
 # On commence par creer le tag que prendra le virtual screen.
-herbstclient add rbtm
+#herbstclient add rbtm
+create_new_panel rbrm
 
 # On cree les monitors
-herbstclient add_monitor 384x1080+1536+0 rbtm onRightVirtualBottom
+#herbstclient add_monitor 384x1080+1536+0 rbtm onRightVirtualBottom
+create_new_panel 384x1080+1536+0 rbtm onRightVirtualBottom
 
 # On ajoute les differents padding pour que les virtuals screens ne se chevauchent pas
 # client		function	name					padding
 #													up		right	bottom	left
 # 384 + 16 = 400
-herbstclient	pad			develop					16		384
+#herbstclient	pad			develop					16		384
+padding_panel develop 16 384
 
 # Si le virtual screen en haut a droite est actif alors on ajoute un padding de 200 pixels supplementaires,
 # Sinon on fait seulement un padding de 16 pixels pour dzen
-if [ -z "$(herbstclient list_monitors | grep -i ontopvirtualright | cut -d ',' -f 2 | cut -d '"' -f 2)" ]; then
-	herbstclient	pad			onRightVirtualBottom	16		0		0		0
+#if [ -z "$(herbstclient list_monitors | grep -i ontopvirtualright | cut -d ',' -f 2 | cut -d '"' -f 2)" ]; then
+#	herbstclient	pad			onRightVirtualBottom	16		0		0		0
+#else
+#	herbstclient	pad			onRightVirtualBottom	216		0		0		0
+#fi
+
+if [ "$(is_panel_active onRightVirtualBottom)" -eq 1 ]; then
+	padding_panel onRightVirtualBottom 16 0 0 0
 else
-	herbstclient	pad			onRightVirtualBottom	216		0		0		0
+	padding_panel onRightVirtualBottom 216 0 0 0
 fi
 
 exit 0
