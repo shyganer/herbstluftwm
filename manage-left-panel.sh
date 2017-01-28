e9cf907a2b8324eb132c663bcf1b582814befe5e
Benjamin Guy-Sotteau <benjamin.guysotteau@outlook.com>
2017-01-28 15:12:57 +0100

    manage-left-panel change own directory

--- manage-left-panel.sh
+++ manage-left-panel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source /home/shyganer/b2c/config/herbstluftwm/musthave_functions.sh
+source /home/shyganer/b2c/my-own/herbstluftwm/musthave_functions.sh
 
 if $(is_panel_active onLeftVirtualBottom) = true; then
-	/home/shyganer/b2c/config/herbstluftwm/remove-left-panel.sh
+	/home/shyganer/b2c/my-own/herbstluftwm/remove-left-panel.sh
 else
-	/home/shyganer/b2c/config/herbstluftwm/left-panel.sh
+	/home/shyganer/b2c/my-own/herbstluftwm/left-panel.sh
 fi
 
 exit 0
